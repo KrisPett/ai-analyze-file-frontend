fa211fc6c14d286fb535db0126b13199bfbd5d4e
KrisPett <70701718+KrisPett@users.noreply.github.com>
2025-01-07 09:44:04 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AnalyzeResultViewComponent } from './features/analyze-result-view/analyze-result-view.component';
 import { UploadFileViewComponent } from './features/upload-file-view/upload-file-view.component';
-import {HomeViewComponent} from './home-view/home-view.component';
+import {HomeViewComponent} from './features/home-view/home-view.component';
 
 export const routes: Routes = [
     { path: '', component: HomeViewComponent },
